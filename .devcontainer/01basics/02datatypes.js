cb0e9ea02ecfb0d60059691b301cd98e074f7d91
Mustakeen Nimbargi <113852166+Mustakeen-777@users.noreply.github.com>
2025-07-29 18:30:07 +0000

    Datatypes

---
+++ .devcontainer/01basics/02datatypes.js
@@ -0,0 +1,26 @@
+" use strict";  // treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); // ; is just to separate they two different logs
+console.log("Mustakeen")
+// code readability should be high
+ 
+let name = "mustakeen"
+let age =18
+let isLoggedIn = false;
+
+
+// number - 2 to power 53
+// bigint
+// string "" or ''
+// boolean - true/false
+// null - standalone value
+// undefined
+// symbol this highly used in the react - unique
+// object
+console.log(typeof "Mustakeen");
+console.log(typeof age);
+console.log(typeof null); //imp for interview
+console.log(typeof undefined);
+
+
