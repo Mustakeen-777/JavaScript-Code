c4843f8a94f9a4736c2b0a635a457c3fbf500ef8
Mustakeen Nimbargi <113852166+Mustakeen-777@users.noreply.github.com>
2025-08-06 17:06:15 +0000

    Strings

--- .devcontainer/01basics/05_Strings.js
+++ .devcontainer/01basics/05_Strings.js
@@ -3,3 +3,23 @@ const repoCount =50;
 console.log(name+ repoCount+  "value");
 
 console.log("hello my  name is ${name} and my repo Count is ${repoCount}");
+
+const gameName = new String("Mustakeen");
+
+console.log(gameName[0]);
+console.log(gameName._proto_);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4);
+console.log(newString);
+
+const newStringOne = "     Hitesh  ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
